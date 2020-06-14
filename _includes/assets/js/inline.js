49dd6ee999814efceb6ec44e33b32ed6bcfd69a5
Gui <guilhermeux@gmail.com>
2020-06-14 12:46:31 +0100

    Homepage and Styling

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,46 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+});
+
+var delay = 1000;
+
+function dot() {
+  var iconDot = document.querySelector(".dot");
+  iconDot.classList.add("dot-typing");
+}
+
+function dots() {
+  var iconDots = document.querySelector(".dots");
+  iconDots.classList.add("dots-typing");
+}
+
+function notif() {
+  var notification = document.querySelector(".notification");
+  notification.classList.add("notif-in");
+}
+
+function text() {
+  var thistext = document.querySelector(".text");
+  thistext.classList.add("text-act");
+}
+
+function newMsg() {
+  setTimeout(function() { dot(); dots(); }, delay);
+  setTimeout(function() { notif(); }, delay*3.6);
+  setTimeout(function() { text(); }, delay*4.2);
+};
+
+newMsg();
+
+window.addEventListener('load', (event) => {
+  console.log('Welcome to my world!');
+
+  tippy('.tooltip', {
+    animation: 'fade',
+    duration: [400, 350]
+  });
+});
--- experience/hey.md
+++
@@ -1,6 +0,0 @@
----
-title: Hey
-date: 2020-06-13T12:42:52.947Z
-summary: Hey
----
-Jey hey
